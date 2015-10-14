3980bbbe423f02b9018d8b63f0e92086c344e5df
wdavilaneto <wdavilaneto@gmail.com>
2015-10-14 12:51:44 -0300

    nope

--- src/client/JarvisApplication.cpp
+++ src/client/JarvisApplication.cpp
@@ -11,11 +11,11 @@ JarvisApplication::~JarvisApplication(void) { }
 
 void JarvisApplication::createScene() {
 
-//    mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
-//
-//    Ogre::Light *light = mSceneMgr->createLight("MainLight");
-//    light->setPosition(20, 80, 50);
-//
-//    Ogre::SceneNode* brainPart = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
+
+    Ogre::Light *light = mSceneMgr->createLight("MainLight");
+    light->setPosition(20, 80, 50);
+
+    Ogre::SceneNode* brainPart = mSceneMgr->getRootSceneNode()->createChildSceneNode();
 
 }
---
+++ src/repository/NetworkRepository.hpp
@@ -0,0 +1,17 @@
+//
+// Created by walter on 11/10/15.
+//
+
+#ifndef JARVIS_NETWORKREPOSITORY_HPP
+#define JARVIS_NETWORKREPOSITORY_HPP
+
+
+class NetworkRepository {
+public:
+
+
+
+};
+
+
+#endif //JARVIS_NETWORKREPOSITORY_HPP
