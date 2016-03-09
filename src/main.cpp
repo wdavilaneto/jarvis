d72fb2a1197f771f0965df8175f6049b2ad69d3f
wdavilaneto <wdavilaneto@gmail.com>
2016-03-09 17:42:55 -0300

    acerto do cmake e testes do Builder

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,20 @@ int main(int arg, char *argv[]) {
 
     http2 server;
 
+    server.num_threads(4);
+
     server.handle("/", [](const request &req, const response &res) {
         res.write_head(200);
         res.end("hello, world\n");
     });
 
-    boost::system::error_code ec;
-    if (server.listen_and_serve(ec, "0.0.0.0", "8080")) {
-        std::cerr << "error: " << ec.message() << std::endl;
+    try {
+        boost::system::error_code ec;
+        if (server.listen_and_serve(ec, "localhost", "8000")) {
+            std::cerr << "error: " << ec.message() << std::endl;
+        }
+    } catch (const std::exception &e) {
+        std::cout << e.what() << std::endl;
     }
-    return EXIT_SUCCESS;
+
 };
