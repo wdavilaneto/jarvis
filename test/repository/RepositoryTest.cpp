22b4dd155fd38c7a8dc61d651cf3500c22358c07
wdavilaneto <wdavilaneto@gmail.com>
2016-06-30 19:35:19 -0300

    oostboost

--- test/repository/RepositoryTest.cpp
+++ test/repository/RepositoryTest.cpp
@@ -8,28 +8,34 @@
 #include <boost/property_tree/ini_parser.hpp>
 
 #include <iostream>
-#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(RepositoryTest) {
+BOOST_AUTO_TEST_SUITE(test_suite1)
 
-    #define CONFIG_FILE_PATH "../../resources/application.cfg"
+    BOOST_AUTO_TEST_CASE(repository_test_case_1) {
 
-    using namespace std;
-    BOOST_TEST_MESSAGE("Repository toolkit tests");
+        #define CONFIG_FILE_PATH "../../resources/application.cfg"
 
-    try {
-        boost::property_tree::ptree config;
-        boost::property_tree::ini_parser::read_ini(CONFIG_FILE_PATH, config);
-        soci::session session(config.get<string>("database.dbname"), config.get<string>("database.connection"));
+        using namespace std;
+        BOOST_TEST_MESSAGE("Repository toolkit tests");
 
-        dto::text_data text_data;
-        long total = 0;
-        session << "select count(*) from bi_manifestacao where nr_mp = :1", soci::into(total), soci::use(201100742214);
-        BOOST_CHECK_GT(total, 0);
-        cout << total << '\n';
+        try {
 
-    } catch (exception const &e) {
-        std::cerr << "Error: " << e.what() << '\n';
-    }
+            boost::property_tree::ptree config;
+            boost::property_tree::ini_parser::read_ini(CONFIG_FILE_PATH, config);
+            soci::session session(config.get<string>("database.dbname"), config.get<string>("database.connection"));
 
-};
+            dto::text_data text_data;
+            // BI_MANIFESTACAO ( nr_mp , tipo_movimento , destino, movimento , pdf )
+            session << "select pdf from bi_manifestacao where nr_mp = :1", soci::into(text_data.text), soci::use(
+                201100742214);
+            //BOOST_CHECK_GT(total, 0);
+            cout << text_data.text << '\n';
+
+        } catch (exception const &e) {
+            std::cerr << "Error: " << e.what() << '\n';
+        }
+
+    };
+
+BOOST_AUTO_TEST_SUITE_END()
