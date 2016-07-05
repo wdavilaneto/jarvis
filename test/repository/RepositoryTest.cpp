a9cb6cfc0147dc36dcb806d13382fd94a2737c87
wdavilaneto <wdavilaneto@gmail.com>
2016-07-05 19:37:46 -0300

    oostboost

--- test/repository/RepositoryTest.cpp
+++ test/repository/RepositoryTest.cpp
@@ -5,19 +5,24 @@
 #include "soci/soci.h"
 #include "soci/sqlite3/soci-sqlite3.h"
 #include "soci/postgresql/soci-postgresql.h"
+
+#include <boost/filesystem.hpp>
+#include <iostream>
 #include <boost/property_tree/ini_parser.hpp>
 
-#include <iostream>
 #include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(test_suite1)
 
     BOOST_AUTO_TEST_CASE(repository_test_case_1) {
 
-        #define CONFIG_FILE_PATH "../../resources/application.cfg"
+        #define CONFIG_FILE_PATH "resources/application.cfg"
 
         using namespace std;
-        BOOST_TEST_MESSAGE("Repository toolkit tests");
+
+        boost::filesystem::path full_path(boost::filesystem::current_path());
+
+        BOOST_TEST_MESSAGE(string() << full_path);
 
         try {
 
@@ -27,8 +32,7 @@ BOOST_AUTO_TEST_SUITE(test_suite1)
 
             dto::text_data text_data;
             // BI_MANIFESTACAO ( nr_mp , tipo_movimento , destino, movimento , pdf )
-            session << "select pdf from bi_manifestacao where nr_mp = :1", soci::into(text_data.text), soci::use(
-                201100742214);
+            session << "select pdf from bi_manifestacao where nr_mp = :1", soci::into(text_data.text), soci::use(201100742214);
             //BOOST_CHECK_GT(total, 0);
             cout << text_data.text << '\n';
 
